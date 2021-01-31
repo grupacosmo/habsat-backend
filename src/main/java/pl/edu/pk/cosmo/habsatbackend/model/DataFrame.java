5c02829f1e597b66156a16e5e87794cfd0325060
Patryk Borchowiec <patryk.borchowiec@jlupin.com>
2021-01-31 14:16:06 +0100

    Added sending generated frames. Added dateTime field to DataFrame.

--- src/main/java/pl/edu/pk/cosmo/habsatbackend/model/DataFrame.java
+++ src/main/java/pl/edu/pk/cosmo/habsatbackend/model/DataFrame.java
@@ -2,18 +2,22 @@ package pl.edu.pk.cosmo.habsatbackend.model;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
 public class DataFrame {
     private String id;
+    private LocalDateTime dateTime;
     private double heightInMeters;
     private double temperatureInCelsius;
     private double longitude;
     private double latitude;
 
-    public DataFrame(double heightInMeters, double temperatureInCelsius, double longitude, double latitude) {
+    public DataFrame(LocalDateTime dateTime, double heightInMeters, double temperatureInCelsius, double longitude,
+                     double latitude) {
         this.id = UUID.randomUUID().toString();
+        this.dateTime = dateTime;
         this.heightInMeters = heightInMeters;
         this.temperatureInCelsius = temperatureInCelsius;
         this.longitude = longitude;
--- src/main/java/pl/edu/pk/cosmo/habsatbackend/service/DataFrameService.java
+++ src/main/java/pl/edu/pk/cosmo/habsatbackend/service/DataFrameService.java
@@ -3,17 +3,47 @@ package pl.edu.pk.cosmo.habsatbackend.service;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import pl.edu.pk.cosmo.habsatbackend.model.DataFrame;
+
+import java.time.LocalDateTime;
+import java.util.Random;
 
 @Service
 public class DataFrameService {
     private final SimpMessagingTemplate simpMessagingTemplate;
+    private DataFrame currentDataFrame;
+    private static final int DELAY_IN_SEC = 5;
 
     public DataFrameService(SimpMessagingTemplate simpMessagingTemplate) {
         this.simpMessagingTemplate = simpMessagingTemplate;
+        this.currentDataFrame = new DataFrame(LocalDateTime.now(), 1000, 20, 20,
+                50);
     }
 
-    @Scheduled(fixedDelay = 1000)
+    @Scheduled(fixedDelay = 1000 * DELAY_IN_SEC)
     public void sendTestFrame() {
-        simpMessagingTemplate.convertAndSend("/data/ws","hehe");
+        Random random = new Random();
+
+        currentDataFrame = new DataFrame(
+                LocalDateTime.now(),
+                currentDataFrame.getHeightInMeters() + random.nextDouble() * 2 * DELAY_IN_SEC,
+                20 + random.nextDouble(),
+                currentDataFrame.getLongitude() + random.nextDouble() * DELAY_IN_SEC,
+                currentDataFrame.getLatitude() + random.nextDouble() * DELAY_IN_SEC
+        );
+
+        if (currentDataFrame.getLatitude() > 90) {
+            currentDataFrame.setLatitude(currentDataFrame.getLatitude() - 180);
+        }
+
+        if (currentDataFrame.getLongitude() > 180) {
+            currentDataFrame.setLongitude(currentDataFrame.getLongitude() - 360);
+        }
+
+        if (currentDataFrame.getHeightInMeters() > 30000) {
+            currentDataFrame.setHeightInMeters(1000);
+        }
+
+        simpMessagingTemplate.convertAndSend("/data/ws",currentDataFrame);
     }
 }
